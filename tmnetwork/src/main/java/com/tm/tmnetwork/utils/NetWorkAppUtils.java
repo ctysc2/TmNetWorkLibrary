d09df7d702834bf52ec399ee34b35f27443fcfb1
cty <cty413159589@163.com>
2023-04-24 16:52:54 +0800

    fix

--- tmnetwork/src/main/java/com/tm/tmnetwork/utils/NetWorkAppUtils.java
+++ tmnetwork/src/main/java/com/tm/tmnetwork/utils/NetWorkAppUtils.java
@@ -3,12 +3,16 @@ package com.tm.tmnetwork.utils;
 import com.tm.tmnetwork.Const;
 import com.tm.tmnetwork.app.TmsLibraryApp;
 
-public class AppUtils {
+public class NetWorkAppUtils {
     public static String getUrlConfig(){
         return PreferenceUtils.getPrefString(TmsLibraryApp.getInstances(), Const.KEY_URL_CONFIG,"");
     }
 
     public static String getAccountId(){
         return PreferenceUtils.getPrefString(TmsLibraryApp.getInstances(),Const.KEY_ACCOUNT_ID,"");
+    }
+
+    public static String getTenantId(){
+        return PreferenceUtils.getPrefString(TmsLibraryApp.getInstances(),Const.KEY_TENANT_ID,"");
     }
 }
